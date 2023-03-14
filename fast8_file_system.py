6002a3644c7ffc5dd5a3a1dd7b30e35a930bca1a
fast8saim <127437230+fast8saim@users.noreply.github.com>
2023-03-14 18:44:22 +0300

    Baselist reading bug fixed

--- fast8_file_system.py
+++ fast8_file_system.py
@@ -2,8 +2,11 @@
 
 import os
 
+
 def get_ibases_content():
-    file_bases = os.getcwd() + "\\Application Data\\1C\\1CEStart\\ibases.v8i"
+    home_dir = os.environ['USERPROFILE']
+    file_bases = home_dir + "\\Application Data\\1C\\1CEStart\\ibases.v8i"
+    print(file_bases)
     if os.path.exists(file_bases):
         text = open(file_bases, encoding = "utf").readlines()
     else:
